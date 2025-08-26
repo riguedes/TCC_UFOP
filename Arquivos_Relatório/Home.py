7a40733e4c46abfeca7460266d9c23fd8599d856
Rian Guedes <ryannguedes1709@gmail.com>
2025-08-26 12:56:22 -0300

    Update Home.py

--- Arquivos_Relatório/Home.py
+++ Arquivos_Relatório/Home.py
@@ -10,9 +10,9 @@ st.set_page_config(
 )
 
 # --- Carregamento dos dados ---
-df = pd.read_csv("songs_info.csv")
-df_um = pd.read_csv("artistas_popularidade.csv")
-df_dois = pd.read_csv("artistas_info.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/riguedes/TCC_UFOP/refs/heads/main/Arquivos_Relat%C3%B3rio/songs_info.csv")
+df_um = pd.read_csv("https://raw.githubusercontent.com/riguedes/TCC_UFOP/refs/heads/main/Arquivos_Relat%C3%B3rio/artistas_popularidade.csv")
+df_dois = pd.read_csv("https://raw.githubusercontent.com/riguedes/TCC_UFOP/refs/heads/main/Arquivos_Relat%C3%B3rio/artistas_info.csv")
 
 # --- Barra Lateral (Filtros) ---
 st.sidebar.header("🔍 Filtros")
