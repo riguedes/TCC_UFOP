f18dc7ebbe3c4f47b060aa581e680f49bf9d3128
Rian Guedes <ryannguedes1709@gmail.com>
2025-09-02 20:44:52 -0300

    Update Home.py

--- Inferify/Home.py
+++ Inferify/Home.py
@@ -9,7 +9,7 @@ st.set_page_config(
 )
 
 # --- Carregamento dos dados ---
-df = pd.read_csv("songs_info.csv")
+df = pd.read_csv(https://raw.githubusercontent.com/riguedes/TCC_UFOP/refs/heads/main/Inferify/songs_info.csv)
 df_um = pd.read_csv(https://raw.githubusercontent.com/riguedes/TCC_UFOP/refs/heads/main/Inferify/artistas_popularidade.csv)
 df_dois = pd.read_csv(https://raw.githubusercontent.com/riguedes/TCC_UFOP/refs/heads/main/Inferify/artistas_info.csv)
 
